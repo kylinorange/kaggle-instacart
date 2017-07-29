9391d2275d0f31ed8683e6129cbc1ade7c9e6153
You Jia <jiayou50@gmail.com>
2017-07-29 16:49:52 -0700

    v1: days since last order (up) and cat features, 5 fold training with average ensemble

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change Log
+
+v1: days since last order (up) and cat features, 5 fold training with average ensemble
+
+v0: public kernel repro
---
+++ utils/plotting.py
@@ -0,0 +1,102 @@
+from xgboost.core import Booster
+from xgboost.sklearn import XGBModel
+
+
+def plot_importance(booster, ax=None, height=0.2,
+                    xlim=None, ylim=None, title='Feature importance',
+                    xlabel='F score', ylabel='Features',
+                    importance_type='weight', max_num_features=None,
+                    grid=True, **kwargs):
+
+    """Plot importance based on fitted trees.
+    Parameters
+    ----------
+    booster : Booster, XGBModel or dict
+        Booster or XGBModel instance, or dict taken by Booster.get_fscore()
+    ax : matplotlib Axes, default None
+        Target axes instance. If None, new figure and axes will be created.
+    importance_type : str, default "weight"
+        How the importance is calculated: either "weight", "gain", or "cover"
+        "weight" is the number of times a feature appears in a tree
+        "gain" is the average gain of splits which use the feature
+        "cover" is the average coverage of splits which use the feature
+            where coverage is defined as the number of samples affected by the split
+    max_num_features : int, default None
+        Maximum number of top features displayed on plot. If None, all features will be displayed.
+    height : float, default 0.2
+        Bar height, passed to ax.barh()
+    xlim : tuple, default None
+        Tuple passed to axes.xlim()
+    ylim : tuple, default None
+        Tuple passed to axes.ylim()
+    title : str, default "Feature importance"
+        Axes title. To disable, pass None.
+    xlabel : str, default "F score"
+        X axis title label. To disable, pass None.
+    ylabel : str, default "Features"
+        Y axis title label. To disable, pass None.
+    kwargs :
+        Other keywords passed to ax.barh()
+    Returns
+    -------
+    ax : matplotlib Axes
+    """
+    # TODO: move this to compat.py
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        raise ImportError('You must install matplotlib to plot importance')
+
+    if isinstance(booster, XGBModel):
+        importance = booster.get_booster().get_score(importance_type=importance_type)
+    elif isinstance(booster, Booster):
+        importance = booster.get_score(importance_type=importance_type)
+    elif isinstance(booster, dict):
+        importance = booster
+    else:
+        raise ValueError('tree must be Booster, XGBModel or dict instance')
+
+    if len(importance) == 0:
+        raise ValueError('Booster.get_score() results in empty')
+
+    tuples = [(k, importance[k]) for k in importance]
+    if max_num_features is not None:
+        tuples = sorted(tuples, key=lambda x: x[1])[-max_num_features:]
+    else:
+        tuples = sorted(tuples, key=lambda x: x[1])
+    labels, values = zip(*tuples)
+
+    if ax is None:
+        _, ax = plt.subplots(1, 1)
+
+    ylocs = np.arange(len(values))
+    ax.barh(ylocs, values, align='center', height=height, **kwargs)
+
+    for x, y in zip(values, ylocs):
+        ax.text(x + 1, y, x, va='center', fontsize=15)
+
+    ax.set_yticks(ylocs)
+    ax.set_yticklabels(labels, fontsize=15)
+
+    if xlim is not None:
+        if not isinstance(xlim, tuple) or len(xlim) != 2:
+            raise ValueError('xlim must be a tuple of 2 elements')
+    else:
+        xlim = (0, max(values) * 1.1)
+    ax.set_xlim(xlim)
+
+    if ylim is not None:
+        if not isinstance(ylim, tuple) or len(ylim) != 2:
+            raise ValueError('ylim must be a tuple of 2 elements')
+    else:
+        ylim = (-1, len(values))
+    ax.set_ylim(ylim)
+
+    if title is not None:
+        ax.set_title(title)
+    if xlabel is not None:
+        ax.set_xlabel(xlabel)
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    ax.grid(grid)
+    return ax
