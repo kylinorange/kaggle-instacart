e21fd3091de426b7d198c6f2245bb162e03741fa
Jun Xie <kylinorange@gmail.com>
2017-08-11 12:12:35 -0700

    full SB features

--- utils/data.py
+++ utils/data.py
@@ -60,25 +60,112 @@ schema = {
     'up_order_since_last_not_order': np.float16
 }
 
-class Data:
-    @staticmethod
-    def merge_product_embeddings(df):
-        pe = pd.read_pickle(os.path.join(root, 'abt-share', 'feature.product_embeddings.pkl'))
-        pe = pe[['product_id'] + [str(i) for i in range(32)]]
-        df = df.merge(pe, on='product_id', how='left')
-        return df
+pe = pd.read_pickle(os.path.join(root, 'abt-share', 'feature.product_embeddings.pkl'))
+pe = pe[['product_id'] + [i for i in range(32)]]
+pe.columns = [str(c) for c in pe.columns]
 
+def product_embeddings():
+    global pe
+    return pe
+
+sb_features = [
+    # 'reordered_dow_ration', 'reordered_dow', 'reordered_dow_size',
+    # 'reordered_prev', 'add_to_cart_order_prev', 'order_dow_prev', 'order_hour_of_day_prev',
+    'user_product_reordered_ratio', 'reordered_sum',
+    'add_to_cart_order_inverted_mean', 'add_to_cart_order_relative_mean',
+    # 'reorder_prob',
+    'last', 'prev1', 'prev2', 'median', 'mean',
+    'dep_reordered_ratio', 'aisle_reordered_ratio',
+    'aisle_products',
+    'aisle_reordered',
+    'dep_products',
+    'dep_reordered',
+    # 'prod_users_unq', 'prod_users_unq_reordered',
+    # 'order_number',
+    # 'prod_add_to_card_mean',
+    # 'days_since_prior_order',
+    'order_dow', 'order_hour_of_day',
+    # 'reorder_ration',
+    # 'user_orders',
+    # 'user_order_starts_at',
+    # 'user_mean_days_since_prior',
+    # 'user_median_days_since_prior',
+    # 'user_average_basket',
+    # 'user_distinct_products',
+    'user_reorder_ratio',
+    # 'user_total_products',
+    # 'prod_orders', 'prod_reorders',
+    # 'up_order_rate',
+    # 'up_orders_since_last_order',
+    # 'up_order_rate_since_first_order',
+    # 'up_orders',
+    # 'up_first_order', 'up_last_order',
+    # 'up_mean_cart_position',
+    # 'up_median_cart_position',
+    'days_since_prior_order_mean',
+    # 'days_since_prior_order_median',
+    'order_dow_mean',
+    # 'order_dow_median',
+    'order_hour_of_day_mean',
+    # 'order_hour_of_day_median'
+] + [str(i) for i in range(32)]
+
+def up_interval_stats(augname=None):
+    if augname is None:
+        dfs = []
+        for sid in range(64):
+            dfs.append(pd.read_pickle(os.path.join(root, 'abt-share', 'feature.up_interval_stat.orig{}.pkl'.format(sid))))
+        df = pd.concat(dfs, ignore_index=True)
+    else:
+        df = pd.read_pickle(os.path.join(root, 'abt', 'feature.up_interval_stat.{}.pkl'.format(augname)))
+    return df
+
+def sb_prune(df):
+    df = df[sb_features + ['order_id', 'product_id']]
+    names = {}
+    for old_name in sb_features:
+        names[old_name] = "sb_{}".format(old_name)
+    df.rename(columns=names)
+    return df
+
+def sb_train(augname=None):
+    if augname is None:
+        dfs = []
+        for sid in range(64):
+            df = pd.read_pickle(os.path.join(root, 'abt-share', 'abt_train.sb.orig{}.pkl'.format(sid)))
+            dfs.append(df)
+        train = pd.concat(dfs, ignore_index=True)
+    else:
+        train = pd.read_pickle(os.path.join(root, 'abt', 'abt_train.sb.{}.pkl'.format(augname)))
+
+    train = train.merge(up_interval_stats(augname), on=['product_id', 'user_id'], how='left')
+    train = train.merge(product_embeddings(), on='product_id', how='left')
+    return sb_prune(train)
+
+def sb_test():
+    dfs = []
+    for sid in range(64):
+        df = pd.read_pickle(os.path.join(root, 'abt-share', 'abt_test.sb.orig{}.pkl'.format(sid)))
+        dfs.append(df)
+    test = pd.concat(dfs, ignore_index=True)
+    test = test.merge(up_interval_stats(), on=['product_id', 'user_id'], how='left')
+    test = test.merge(product_embeddings(), on='product_id', how='left')
+    return sb_prune(test)
+
+class Data:
     @staticmethod
     def train_aug(down_sample=None):
         dfs = []
         for s in range(32):
             for ms in range(2):
+                augname = 'aug{}-{}'.format(s, ms)
                 df = pd.read_csv(
-                    os.path.join(root, 'abt', 'abt_train.aug{}-{}.csv'.format(s, ms)),
+                    os.path.join(root, 'abt', 'abt_train.{}.csv'.format(augname)),
                     dtype=schema)
                 if down_sample is not None:
                     df = df[df.order_id % down_sample == 0]
                 df['aug'] = 1
+                df = df.merge(sb_train(augname=augname), how='left', on=['order_id', 'product_id'])
                 dfs.append(df)
         return dfs
 
@@ -90,6 +177,7 @@ class Data:
         if down_sample is not None:
             train = train[train.order_id % down_sample == 0]
         train['aug'] = 0
+        train = train.merge(sb_train(augname=None), how='left', on=['order_id', 'product_id'])
 
         if aug:
             train_aug = Data.train_aug(down_sample=down_sample)
@@ -97,7 +185,6 @@ class Data:
 
         train.loc[:, 'reordered'] = train.reordered.fillna(0)
         Data.random_feature(train)
-        train = Data.merge_product_embeddings(train)
         train.sort_index(axis=1, inplace=True)
 
         return train
@@ -123,8 +210,8 @@ class Data:
             test = test[test.order_id % down_sample == 0]
 
         test['aug'] = 0
+        test = test.merge(sb_test(), how='left', on=['order_id', 'product_id'])
         Data.random_feature(test)
-        test = Data.merge_product_embeddings(test)
         test.sort_index(axis=1, inplace=True)
 
         return test
